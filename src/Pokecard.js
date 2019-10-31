526b070322a5530ec2c1f9cface035b7fb058617
Ryan Wiggins <ryanwiggins52@gmail.com>
2019-10-30 20:12:15 -0400

    added basic css styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -3,14 +3,14 @@ import './Pokecard.css'
 const Poke_API =
   'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 class Pokecard extends Component {
-  render () {
+  render() {
     const imgSrc = `${Poke_API}${this.props.id}.png`
     return (
       <div className='Pokecard'>
-        <h1>{this.props.name}</h1>
+        <h1 className='Pokecard-title'>{this.props.name}</h1>
         <img src={imgSrc} alt={`Here is supposed to be ${this.props.name}`} />
-        <div>Type: {this.props.type}</div>
-        <div>EXP: {this.props.exp}</div>
+        <div className='Pokecard-data'>Type: {this.props.type}</div>
+        <div className='Pokecard-data'>EXP: {this.props.exp}</div>
       </div>
     )
   }
